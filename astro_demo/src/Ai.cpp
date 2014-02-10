7a5c04ec0f31d8722b1f73f8c929c1cf992bb9fc
SpectralRenegade <shanemgeller@gmail.com>
2014-02-10 14:01:07 -0500

    Removed Debug Message for item pickups

--- astro_demo/src/Ai.cpp
+++ astro_demo/src/Ai.cpp
@@ -155,7 +155,7 @@ void PlayerAi::handleActionKey(Actor *owner, int ascii) {
 				if (actor->pickable && actor->x == owner->x && actor->y == owner->y) {
 					if (actor->pickable->pick(actor,owner)) {
 						found = true;
-						engine.gui->message(TCODColor::lightGrey, "You pick up the %s and the sort value is %d.", actor->name, actor->sort);
+						engine.gui->message(TCODColor::lightGrey, "You pick up the %s.", actor->name);
 						break;
 					} else if (!found ) {
 						found = true;
