7298c4e17266449faccb55cbeae005c93740b5e8
Ryan82593 <ryanfaries93@gmail.com>
2014-02-19 17:06:22 -0500

    Allows infectedMarines to drop Battery Packs, in addition to MLRs

--- astro_demo/src/Map.cpp
+++ astro_demo/src/Map.cpp
@@ -704,16 +704,22 @@ void Map::generateRandom(Actor *owner, int ascii){
 	if(dice <= 40){
 			return;
 	}else{
-		if(ascii == 169) //infectedMarines have 60% chance of dropping a MLR
+		if(ascii == 149) //infectedMarines have 60% chance of dropping an item with 50% chance of it being a MLR, and the other 50% chance being a battery pack
 		{
-			//for(int i = 1; i <= owner->container->size; i++)
-			//{
+			if(dice <= 70)
+			{
 				Actor *MLR = createMLR(0,0);
 				engine.actors.push(MLR);
 				MLR->pickable->pick(MLR,owner);
-				//engine.gui->message(TCODColor::white,"MLR added");
-			//}
-				
+			}
+			else
+			{
+				Actor *batt = createBatteryPack(0,0);
+				engine.actors.push(batt);
+				batt->pickable->pick(batt,owner);
+			}
+			
+			
 		}else if(ascii == 164){
 			for(int i = 0; i < owner->container->size; i++){
 				int rndA = rng->getInt(0,100);
