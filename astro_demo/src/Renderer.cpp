b60c5a54aabf5aacbbe51b65edd2104fd9026cf5
Shane Exterkamp <exterkamps@ufl.edu>
2014-02-27 02:22:45 -0500

    Some semi-final changes to the inventory
    On to bigger better projects!

--- astro_demo/src/Renderer.cpp
+++ astro_demo/src/Renderer.cpp
@@ -18,11 +18,13 @@ void Renderer::render(void *sdlSurface){
 	}
 	if (engine.invState == 0)
 	{
+	//set fps
 	TCODSystem::setFps(60);
 	//floors
 	SDL_SetColorKey(screen,SDL_SRCCOLORKEY,255);
 	SDL_Surface *floorTiles = SDL_LoadBMP("tile_assets/tiles.bmp");
 	SDL_SetColorKey(floorTiles,SDL_SRCCOLORKEY,255);
+	//glowing variable alpha set
 	static int alpha = 255*.5;
 	static bool Down = true;
 	if (alpha > 0 && Down)
@@ -51,22 +53,13 @@ void Renderer::render(void *sdlSurface){
 	SDL_Surface *shadows = SDL_LoadBMP("tile_assets/itemShadows.bmp");
 	SDL_SetAlpha(shadows, SDL_SRCALPHA, 255*.25);
 	SDL_SetColorKey(shadows,SDL_SRCCOLORKEY,255);
-	
-
 	//EQUIPMENT
 	SDL_Surface *equipment = SDL_LoadBMP("tile_assets/equipment.bmp");
 	SDL_SetColorKey(equipment,SDL_SRCCOLORKEY,255);
-	
 	//DECORATIONS
 	SDL_Surface *decor = SDL_LoadBMP("tile_assets/decorations.bmp");
 	SDL_SetColorKey(decor,SDL_SRCCOLORKEY,255);
-	
-	
-	//SDL_SetColorKey(humanShadow,SDL_SRCCOLORKEY,255);
-	//background
-	//SDL_Surface *map = SDL_LoadBMP("starmap.bmp");
-	
-	//SDL_Surface *floorMap = SDL_LoadBMP("starmap2_blank.bmp");
+	//Background
 	SDL_Surface *floorMap = SDL_LoadBMP("starmap2.bmp");
 	SDL_Surface *terminal = SDL_LoadBMP("tile_assets/terminal.bmp");
 	SDL_SetColorKey(terminal,SDL_SRCCOLORKEY,255);
@@ -76,7 +69,7 @@ void Renderer::render(void *sdlSurface){
 	
 	
 	
-	///////////////if there is a tile with multiple items, we need to render them both, ask garret how to detect this
+	//messages
 	//engine.gui->message(TCODColor::red, "x1 is %d",engine.mapx1);
 	//engine.gui->message(TCODColor::red, "x2 is %d",engine.mapx2);
 	//engine.gui->message(TCODColor::red, "y1 is %d",engine.mapy1);
@@ -90,41 +83,37 @@ void Renderer::render(void *sdlSurface){
 	int plyx = 0, plyy = 0;
 	for (int xM = engine.mapx1; xM < engine.mapx2+16; xM++) {
 		for (int yM = engine.mapy1; yM < engine.mapy2+16; yM++) {
-			//if it needs to be rendered over, render it over
 			
-			//engine.gui->message(TCODColor::red, "noise  %d",n);
+			//refresh if looking
 			SDL_Rect dstRect={x*16,y*16,16,16};
 			if (TCODConsole::root->getCharBackground(22*16+xM,yM) == TCODColor::darkerPink || //refresh if looking
 				TCODConsole::root->getCharBackground(22*16+xM,yM) == TCODColor::pink)
 			{
 				TCODConsole::root->clear();
 			}
-			
+			//detects where the player is, if it is @, human, alien or robot base
 			if(engine.mapcon->getChar(xM,yM) == 64 || engine.mapcon->getChar(xM,yM) == 143 ||
      		  engine.mapcon->getChar(xM,yM) == 159 || engine.mapcon->getChar(xM,yM) == 175)
 			{
 				plyx = x;
 				plyy = y;
+				//refresh if not deadz  (163 is corpse)
 				if (engine.mapcon->getChar(xM,yM) != 163){//|| engine.gameStatus != engine.MAIN_MENU){ //refresh if player is alive
 					TCODConsole::root->clear();	
 				}
 				
 			}
-			//SDL_Rect dstRect2={0,0,0,0};
-			//SDL_Rect srcRect = {};
+			//if the tile is a floor
 			if(engine.mapconCpy->getChar(xM,yM) == 31) 
 			{ //replace 'down arrow thing' (31 ascii) with basic floor tiles
-				//SDL_UpdateRect(floorMap, x*16, y*16, 16, 16);
-				//SDL_FillRect(floorMap, &dstRect, 258);
-				//Map::RoomType r = engine.map->tiles[xM*yM].tileType;
 				
 				//office floors, can test for any floors
 				int r = engine.map->tileType(xM,yM);
-				if (r == 2)
+				if (r == 2)//2 is office
 				{
 					srcRect.x = 48;
 				}
-				else
+				else //else is regular floors
 				{
 					srcRect.x = 0;
 				}
@@ -142,8 +131,10 @@ void Renderer::render(void *sdlSurface){
 				if (engine.mapconDec->getChar(xM,yM) == ' ')
 				{
 					//do nothing if common case (i.e.) no decor
+					//commmon case is fast
 				}
-				else if (engine.mapconDec->getChar(xM,yM) == 5 || engine.mapconDec->getChar(xM,yM) == 6 ||  //papers
+				//if the decor is papers, render over
+				else if (engine.mapconDec->getChar(xM,yM) == 5 || engine.mapconDec->getChar(xM,yM) == 6 || 
 						 engine.mapconDec->getChar(xM,yM) == 7 || engine.mapconDec->getChar(xM,yM) == 8)
 				{
 					srcRect.y = 32;
@@ -183,9 +174,9 @@ void Renderer::render(void *sdlSurface){
 			//replace 'up arrow thing' with darker floor tiles
 			else if(engine.mapconCpy->getChar(xM,yM) == 30)
 			{
-				//render office floors
+				//render office floors, same as lit tiles, just different y value
 				int r = engine.map->tileType(xM,yM);
-				if (r == 2)
+				if (r == 2) 
 				{
 					srcRect.x = 48;
 				}
@@ -196,7 +187,7 @@ void Renderer::render(void *sdlSurface){
 				srcRect.y = 16;
 				SDL_BlitSurface(floorTiles,&srcRect,floorMap,&dstRect);
 				
-				//add infection
+				//add infection, same a lit, except y
 				if (engine.map->isInfected(xM,yM))
 				{
 					srcRect.x = 16;
@@ -208,8 +199,9 @@ void Renderer::render(void *sdlSurface){
 				if (engine.mapconDec->getChar(xM,yM) == ' ')
 				{
 					//do nothing if common case (i.e.) no decor
+					//common case is fast
 				}
-				else if (engine.mapconDec->getChar(xM,yM) == 5 || engine.mapconDec->getChar(xM,yM) == 6 ||  //papers
+				else if (engine.mapconDec->getChar(xM,yM) == 5 || engine.mapconDec->getChar(xM,yM) == 6 ||  //dark papers
 						 engine.mapconDec->getChar(xM,yM) == 7 || engine.mapconDec->getChar(xM,yM) == 8)
 				{
 					srcRect.y = 48;
@@ -218,44 +210,7 @@ void Renderer::render(void *sdlSurface){
 								
 				}
 			}
-			//replace infected tiles lit
-			/*else if(engine.mapconCpy->getChar(xM,yM) == 29){
-				//SDL_FillRect(floorMap, &dstRect, 258);
-				srcRect.x = 16;
-				srcRect.y = 0;
-				SDL_BlitSurface(floorTiles,&srcRect,floorMap,&dstRect);
-				//SDL_FillRect(floorMap, &dstRect, 258);
-				if (engine.mapcon->getChar(xM,yM) == 181 || engine.mapcon->getChar(xM,yM) == 182 || engine.mapcon->getChar(xM,yM) == 183 || 
-				engine.mapcon->getChar(xM,yM) == 184 || engine.mapcon->getChar(xM,yM) == 64 || engine.mapcon->getChar(xM,yM) == 164 || 
-				engine.mapcon->getChar(xM,yM) == 165 || engine.mapcon->getChar(xM,yM) == 148 || engine.mapcon->getChar(xM,yM) == 132) 
-				{
-					for (Actor **it = engine.actors.begin(); it != engine.actors.end(); it++) {//is walkable?
-						Actor *actor = *it;
-						if (actor->x == xM && actor->y == yM && actor->destructible && actor->destructible->isDead()) {
-							//doubles += 1;
-							SDL_Rect srcRect2={10*16,3*16,16,16};
-							SDL_Rect dstRect={x*16,y*16,16,16};
-							//10 width 3 height for standard bodies
-							//if they are spore bodies
-							if (actor->ch == 162){
-								srcRect2.y = 2*16;
-							}
-							
-							SDL_BlitSurface(terminal,&srcRect2,floorMap,&dstRect);
-						}
-					}
-				}
-			}*/
-			//replace unlit infected tiles
-			//else if(engine.mapconCpy->getChar(xM,yM) == 28){
-			//	srcRect.x = 16;
-			//	srcRect.y = 16;
-			//	SDL_BlitSurface(floorTiles,&srcRect,floorMap,&dstRect);
-			//}
-			
-			//check for doubles
-			
-			
+
 			
 			//OPTIMIZE ME -->  COMMON CASE IS SLOW
 			
@@ -297,10 +252,12 @@ void Renderer::render(void *sdlSurface){
 			{
 				if (engine.mapcon->getCharForeground(xM,yM) == TCODColor::white)
 				{
+					//light
 					srcRect.x = 0;
 				}
 				else
 				{
+					//dark
 					srcRect.x = 16;
 				}
 				if(engine.mapconCpy->getChar(xM,yM-1) == '^')//if wall above it is wall
@@ -325,41 +282,37 @@ void Renderer::render(void *sdlSurface){
 				}
 				SDL_BlitSurface(decor,&srcRect,floorMap,&dstRect);
 			}
-			else if (engine.mapcon->getChar(xM,yM) == 241)//smashed filing cabinet
+			//smashed filing cabinet
+			else if (engine.mapcon->getChar(xM,yM) == 241)
 			{
 				if (engine.mapcon->getCharForeground(xM,yM) == TCODColor::white)
 				{
+					//light
 					srcRect.x = 0;
 				}
 				else
 				{
+					//dark
 					srcRect.x = 16;
 				}
 				srcRect.y = 64;
 				SDL_BlitSurface(decor,&srcRect,floorMap,&dstRect);
 				
 			}
-			else if (engine.mapcon->getChar(xM,yM) == 242)//desk
+			//desks
+			else if (engine.mapcon->getChar(xM,yM) == 242)
 			{
 				if (engine.mapcon->getCharForeground(xM,yM) == TCODColor::white)
 				{
+					//light
 					srcRect.y = 0;
 				}
 				else
 				{
+					//dark
 					srcRect.y = 16;
 				}
-				
-				
-				//int n = xM + yM * 57;
-				//n = (n<<13) ^ n;
-				//float NaN = 7fffffff;
-				//n = 1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589)) / 1073741824.0;   
-				
-				//int n = xM | yM;
-				//int n = xM | yM;
-				//n = n ^ 1376312589;
-				
+				//the different versions to add based on mapconDec's value
 				if(engine.mapconDec->getChar(xM,yM) == 1)
 				{
 					srcRect.x=32+48;
@@ -374,23 +327,20 @@ void Renderer::render(void *sdlSurface){
 					srcRect.x=32;
 				}
 				
-				
-				
 				SDL_BlitSurface(decor,&srcRect,floorMap,&dstRect);
 			}
 			
-			
-			//SDL_Delay(100);
 			y++;
 		}
 		y=0;
 		x++;
 	}
 			
+	//OPTIMIZE ME?  INCLUDE IN PREVIOUS LOOP?
 	int x1 = 0, y1 = 0;
 	for (int xM = engine.mapx1; xM < engine.mapx2+16; xM++) {
 		for (int yM = engine.mapy1; yM < engine.mapy2+16; yM++) {
-			//Player Shadow
+			//Human Shadows
 			int yN = yM - 1;
 			if (engine.mapcon->getChar(xM,yN) == 64 || engine.mapcon->getChar(xM,yN) == 164 || engine.mapcon->getChar(xM,yN) == 148 || engine.mapcon->getChar(xM,yN) == 132)  
 			{
@@ -401,9 +351,7 @@ void Renderer::render(void *sdlSurface){
 				srcRect.x = 32;
 				srcRect.y = 0;
 				SDL_BlitSurface(shadows,&srcRect,floorMap,&dstRectOffset);
-				//SDL_BlitSurface(humanShadow,NULL,floorMap,&dstRectOffset);
-				//SDL_BlitSurface(humanShadow,NULL,screen,&dstRectOffset);
-				//TCODConsole::flush();
+				
 			}
 			
 			
@@ -415,45 +363,21 @@ void Renderer::render(void *sdlSurface){
 	
 	
 	
-	/*
-	//render non static-shadows
-	//if player is not dead //////////////////////////////////////need to make that test
-	int deltaX =0, deltaY = 0;
-	for (int x = mapx1; x <= mapx2; x++) 
-	{
-		for (int y = mapy1; y <= mapy2; y++)
-		{
-			if (TCODConsole::root->getChar(x,y) == 64)  
-			{
-				//convert map's to real x's and y's
-				deltaX = x + ((85-22)/2);
-				deltaY = y + ((47-14)/2);
-			}
-			
-		}
-	}
-	
+/*
+	legacy error messages
 	engine.gui->message(TCODColor::red, "player x is  %d",plyx);
 	engine.gui->message(TCODColor::red, "player y is %d",plyy);
 	engine.gui->message(TCODColor::red, "calc player x is  %d",plyx*16);
 	engine.gui->message(TCODColor::red, "calc player y is  %d",plyy*16);
-	
-	SDL_Rect srcRect2={0,0,32,32};
-	SDL_Rect dstRect11 ={plyx*16+8,plyy*16+8,32,32};
-	//SDL_Rect dstRect2={x1*16-16,y1*16,32,32};                      //shadows do not work when the map stops scrolling?
-	SDL_BlitSurface(humanShadow,&srcRect2,map,&dstRect11);
-	SDL_BlitSurface(humanShadow,&srcRect2,screen,&srcRect2);
-	*/
+*/
 	
 	
 	//SDL_Rect srcRect1={mapx1*16,mapy1*16,(mapx2-mapx1+16)*16,(mapy2-mapy1+16)*16};
-	
-
 	//SDL_BlitSurface(screen,&dstRect1,floorMap,&srcRect1);
 	SDL_BlitSurface(screen,&dstRect1,floorMap,NULL);
 	
 	
-	//COULD OPTIMIZE --> CONTAINER/INVENTORY/PLAYER CONTAINS A LIST OF JUST EQUIPMENT TO RENDER
+	//COULD OPTIMIZE --> CONTAINER/INVENTORY/PLAYER CONTAINS A LIST OF JUST EQUIPMENT TO RENDER, instead of scanning all items
 	SDL_Rect dstRectEquip={plyx*16,plyy*16,16,16};
 	if (engine.gameStatus == engine.IDLE || engine.gameStatus == engine.NEW_TURN){
 		for (Actor **it = engine.player->container->inventory.begin();it != engine.player->container->inventory.end();it++)
@@ -498,23 +422,15 @@ void Renderer::render(void *sdlSurface){
 		}
 	}
 	SDL_UpdateRect(floorMap, plyx*16, plyy*16, 16, 16);
-	//SDL_UpdateRect(screen, plyx*16, plyy*16, 16, 16);
 	
 	
-	//if the game is running add if statement sometime							
-	//SDL_BlitSurface(floorMap,&srcRect1,screen,&dstRect1);	
-	//GOES BLUE ON DEATH, UPDATE ON DEATH status?
+	//if the game is idle or new turn, blit onto screen
 	if (engine.gameStatus == engine.IDLE || engine.gameStatus == engine.NEW_TURN){
 		SDL_BlitSurface(floorMap,NULL,screen,&dstRect1);	
 	}
+	//set everything to dirty
 	TCODConsole::root->setDirty(22*16,0,(engine.mapx2-engine.mapx1)*16+16,(engine.mapy2-engine.mapy1)*16+16);
-	//engine.gui->render();
-	//else
-	//{
-	//	SDL_BlitSurface(titleScreen,NULL,screen,NULL);	
-	//}
-	//SDL_Flip(floorMap);
-
+	//free all surfaces
 	SDL_FreeSurface(floorMap);
 	SDL_FreeSurface(screen);
 	SDL_FreeSurface(floorTiles);
@@ -524,6 +440,7 @@ void Renderer::render(void *sdlSurface){
 	SDL_FreeSurface(terminal);
 	SDL_FreeSurface(decor);
 	}
+	//if inventory is open begin animation
 	else if (engine.invState == 1)
 	{
 	TCODSystem::setFps(60);
@@ -563,7 +480,7 @@ void Renderer::render(void *sdlSurface){
 	}
 	else if (engine.invState == 2)
 	{
-	TCODSystem::setFps(30);
+	TCODSystem::setFps(60);
 	//TCODConsole::root->clear();
 	engine.invFrames++;
 	first=true;
@@ -576,6 +493,8 @@ void Renderer::render(void *sdlSurface){
 	SDL_Surface *tabBig = SDL_LoadBMP("tile_assets/tablet-big.bmp");
 	SDL_SetColorKey(tabBig,SDL_SRCCOLORKEY,255);
 	SDL_Surface *backpack = SDL_LoadBMP("tile_assets/backpack.bmp");
+	SDL_Surface *flap = SDL_LoadBMP("tile_assets/backpack-flap.bmp");
+	SDL_SetColorKey(flap,SDL_SRCCOLORKEY,255);
 	SDL_Surface *bg = SDL_LoadBMP("tile_assets/invBG.bmp");
 	SDL_SetColorKey(backpack,SDL_SRCCOLORKEY,255);
 	//SDL_Rect dstRect1={22*16,0,(engine.mapx2-engine.mapx1)*16+16,(engine.mapy2-engine.mapy1)*16+16};
@@ -597,6 +516,7 @@ void Renderer::render(void *sdlSurface){
 		SDL_BlitSurface(backpack,&dstBack1,screen,&dstBack);
 		SDL_Rect screenTab ={(engine.screenWidth*16)/2-(708/2),48,708,650};
 		SDL_BlitSurface(tabBig,NULL,screen,&screenTab);
+		SDL_FreeSurface(flap);
 		SDL_FreeSurface(backpack);
 		SDL_FreeSurface(tab);
 		SDL_FreeSurface(tabBig);
@@ -604,8 +524,10 @@ void Renderer::render(void *sdlSurface){
 	}
 	else if (engine.invFrames > 45)
 	{
-		TCODSystem::setFps(35);
+		
+		TCODSystem::setFps(45);
 		SDL_BlitSurface(backpack,&dstBack1,screen,&dstBack);
+		
 		SDL_Rect bigTab    ={0,750-(engine.invFrames-45)*22,708,((engine.invFrames-45)*22)};//(16+((engine.invFrames*16)-45))};
 		
 		//SDL_Rect screenTab ={(engine.screenWidth*16)/2-(708/2),48,708,750};
@@ -616,70 +538,99 @@ void Renderer::render(void *sdlSurface){
 		
 		SDL_BlitSurface(tabBig,&bigTab,screen,&screenTab);
 		
-		
+		SDL_FreeSurface(flap);
 		SDL_FreeSurface(backpack);
 		SDL_FreeSurface(tab);
 		SDL_FreeSurface(tabBig);
 		SDL_FreeSurface(bg);
+		
 	}
 	else
 	{
+		TCODSystem::setFps(35);
 		//TCODConsole::root->clear();
 		
 		dstTabS.y -= (engine.invFrames-30)*25;
 		//dstBack.y = 20;
 		SDL_BlitSurface(backpack,&dstBack1,screen,&dstBack);
 		SDL_BlitSurface(tab,&dstTab,screen,&dstTabS);
+		SDL_BlitSurface(flap,&dstBack1,screen,&dstBack);
+		SDL_FreeSurface(flap);
 		SDL_FreeSurface(backpack);
 		SDL_FreeSurface(tab);
 		SDL_FreeSurface(tabBig);
 		SDL_FreeSurface(bg);
+		
 	}
 	}
 	else if (engine.invState == 4)
 	{
+		TCODSystem::setFps(60);
 		TCODConsole::root->clear();
 		
 		first=true;
-		
-		SDL_Rect dstBack={(engine.screenWidth*16)/2-375,(engine.screenHeight*16-48)-(30*16),750,750};
-		SDL_Rect dstBack1={0,0,750,750};
+		//static bool Rend = true;
+		//SDL_Rect dstBack={(engine.screenWidth*16)/2-375,(engine.screenHeight*16-48)-(30*16),750,750};
+		//SDL_Rect dstBack1={0,0,750,750};
 		
 		SDL_Surface *tabBig = SDL_LoadBMP("tile_assets/tablet-big.bmp");
-		SDL_Surface *pointer = SDL_LoadBMP("tile_assets/finger.bmp");
+		//SDL_Surface *pointer = SDL_LoadBMP("tile_assets/finger.bmp");
 		SDL_SetColorKey(tabBig,SDL_SRCCOLORKEY,255);
-		SDL_SetColorKey(pointer,SDL_SRCCOLORKEY,255);
-		SDL_Surface *bg = SDL_LoadBMP("tile_assets/invBG.bmp");
+		//SDL_SetColorKey(pointer,SDL_SRCCOLORKEY,255);
+		//SDL_Surface *bg = SDL_LoadBMP("tile_assets/invBG.bmp");
 		SDL_SetColorKey(screen,SDL_SRCCOLORKEY,0);
 		SDL_Rect screenTab ={(engine.screenWidth*16)/2-(708/2),48,708,650};
 		
-		SDL_Surface *backpack = SDL_LoadBMP("tile_assets/backpack.bmp");
-		SDL_SetColorKey(backpack,SDL_SRCCOLORKEY,255);
+		//SDL_Surface *backpack = SDL_LoadBMP("tile_assets/backpack.bmp");
+		//SDL_SetColorKey(backpack,SDL_SRCCOLORKEY,255);
 		
 		
-		//TCODConsole::root->clear();
-		//SDL_UpdateRect(screen, 0, 0, 85*16,35*16);
 		
 		
+		//SDL_UpdateRect(bg, 0, 0, 0,0);
+		
+		/*static int oldX = 0;
+		static int oldY = 0;
 		int x = engine.selX*16;
 		int y = engine.selY*16;
 		
-		//SDL_BlitSurface(bg,NULL,screen,NULL);
-		//TCODConsole::root->clear();
-		SDL_BlitSurface(backpack,&dstBack1,bg,&dstBack);
-		SDL_BlitSurface(tabBig,NULL,bg,&screenTab);
+		if (oldX != x || oldY != y)
+		{
+			//TCODConosle *temp = new TCODConsole(85,44);
+			TCODConsole::blit(TCODConsole::root, 0, 0, 85, 44, engine.temporary , 0,0);
+			
+			SDL_Surface *tempy =(SDL_Surface *)engine.temporary;
+			SDL_SetColorKey(tempy,SDL_SRCCOLORKEY,0);
+			SDL_BlitSurface(tempy,&screenTab,tabBig,NULL);
+			SDL_BlitSurface(backpack,&dstBack1,bg,&dstBack);
+			SDL_BlitSurface(bg,NULL,screen,NULL);
+			
+			SDL_BlitSurface(tabBig,NULL,screen,&screenTab);
+			oldX = x;
+			oldY = y;
+		}*/
+		//
+		
+		//
+		//SDL_BlitSurface(tabBig,NULL,bg,&screenTab);
+		SDL_BlitSurface(screen,&screenTab,tabBig,NULL);
+		SDL_BlitSurface(tabBig,NULL,screen,&screenTab);
 		//SDL_UpdateRect(screen, 0, 0, 85*16,35*16);
-		SDL_BlitSurface(screen,NULL,bg,NULL);
+		//TCODConsole::root->setDefaultBackground(TCODColor::black);
+		//TCODConsole::root->setDefaultBackground(TCODColor::blue);
+		//TCODConsole::root->clear();
+		//engine.gui->render();
+		//SDL_BlitSurface(screen,NULL,bg,NULL);
 		
-		SDL_Rect pointerBox ={x,y,375,800};
-		//SDL_BlitSurface(pointer,NULL,bg,&pointerBox);
-
+		//SDL_Rect pointerBox ={x,y,375,800};
+		//SDL_BlitSurface(pointer,NULL,screen,&pointerBox);
 		
-		SDL_BlitSurface(bg,NULL,screen,NULL);
-		SDL_FreeSurface(bg);
+		
+		//SDL_BlitSurface(bg,NULL,screen,NULL);
+		//SDL_FreeSurface(bg);
 		SDL_FreeSurface(tabBig);
-		SDL_FreeSurface(backpack);
-		SDL_FreeSurface(pointer);
+		//SDL_FreeSurface(backpack);
+		//SDL_FreeSurface(pointer);
 	}
 	
 }
