a0ad48c7d18ae5ad1683f59cd5045976ee4857b3
SpectralRenegade <shanemgeller@gmail.com>
2014-03-26 11:39:59 -0400

    Character Screen Change
    The character statistics now show your base stat and then any
    armor/weapon bonuses are displayed beside it in parenthesis

--- astro_demo/src/Ai.cpp
+++ astro_demo/src/Ai.cpp
@@ -663,9 +663,9 @@ void PlayerAi::displayCharacterInfo(Actor *owner){
 	con.print(2,4,"STATS");
 	con.print(1,6,"LVL: %d",xpLevel);
 	con.print(1,8,"VIT: %d",owner->vit);
-	con.print(1,10,"DEX: %d",owner->totalDex);
-	con.print(1,12,"STR: %d",owner->totalStr);
-	con.print(1,14,"INT: %d",owner->totalIntel);
+	con.print(1,10,"DEX: %d(+%d)",owner->dex,owner->totalDex-owner->dex);
+	con.print(1,12,"STR: %d(+%d)",owner->str,owner->attacker->totalPower - owner->attacker->basePower);
+	con.print(1,14,"INT: %d(+%d)",owner->intel,owner->totalIntel-owner->intel);
 	con.print(1,16,"KILLS: %d",engine.killCount);
 	//con.print(1,18,"DMG DONE: %g",engine.damageDone);
 	//con.print(1,20,"DMG TAKEN: %g",engine.damageReceived);
